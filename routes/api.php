112473be66f17c83b4618a8f1de7a02597757613
esraamagddi <esraamagdy20133@gmail.com>
2024-07-26 02:52:44 +0300

    feat:add ApiAuth middleware for authntication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\ApiProductController;
+use App\Http\Middleware\ApiAuth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,18 +12,19 @@ Route::get('/user', function (Request $request) {
 
 
 Route::controller(ApiProductController::class)->group(function(){
+    Route::middleware(ApiAuth::class)->group(function(){
 
     Route::get('products',  'allProducts');
     Route::get('products/{id}',  'show');
     Route::post('products',  'store');
     Route::put('products/{id}',  'update');
     Route::delete('products/{id}',  'delete');
-
+    });
 });
 
 Route::controller(ApiAuthController::class)->group(function (){
     Route::post('login',  'login');
     Route::post('register',  'register');
-    Route::post('logout',  'logout');
+    Route::post('logout',  'logout')->middleware(ApiAuth::class);
 
 });
